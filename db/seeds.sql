eeba849bbf5afbae465457c1ce176ce2dd921a50
Arod1207 <arod1207@me.com>
2020-07-22 23:26:03 -0600

    added 3 tables with rating

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,13 +1,22 @@
-INSERT INTO moods
-    (happy, sad, angry, createdAt, updatedAt)
+INSERT INTO Sads
+    (SAD, RATING, createdAt, updatedAt)
 VALUES
-    ("https://open.spotify.com/user/spotify/playlist/37i9dQZF1DXdPec7aLTmlC?si=8ySScaJrR1GJkAF9ZNgAaw", "https://open.spotify.com/user/spotify/playlist/37i9dQZF1DX3YSRoSdA634?si=ayVx39rCSXGlE-bhK57QWA", "https://open.spotify.com/user/spotify/playlist/37i9dQZF1DX1tyCD9QhIWF?si=tNe4BDKlTIyJ19vKbEdLJw", current_date()
+    ('https://open.spotify.com/user/spotify/playlist/37i9dQZF1DX3YSRoSdA634?si=ayVx39rCSXGlE-bhK57QWA', 0, current_date()
 , current_date
 ());
 
-INSERT INTO activities
-    (workout, studying, cooking, commute, createdAt, updatedAt)
+
+INSERT INTO Angries
+    (ANGRY, RATING, createdAt, updatedAt)
 VALUES
-    ("https://open.spotify.com/user/spotify/playlist/37i9dQZF1DX76Wlfdnj7AP?si=7kBDwgZMS0av0yOUTCU5Qg", "https://open.spotify.com/user/spotify/playlist/37i9dQZF1DX8Uebhn9wzrS?si=NboLENckRkq2C-W9UTd2TQ", "https://open.spotify.com/user/spotify/playlist/37i9dQZF1DXb83YJL7gTWj?si=w4KhIqW3RR2e0g4i0XWQLQ", "https://open.spotify.com/user/spotify/playlist/37i9dQZF1DX2MyUCsl25eb?si=Zqdhd8T8QNyiYugvL1B56w", current_date
+    ('https://open.spotify.com/user/spotify/playlist/37i9dQZF1DX1tyCD9QhIWF?si=tNe4BDKlTIyJ19vKbEdLJw', 0, current_date
 (), current_date
-())
+());
+
+INSERT INTO Happies
+    (HAPPY, RATING, createdAt, updatedAt)
+VALUES
+    ('https://open.spotify.com/user/spotify/playlist/37i9dQZF1DXdPec7aLTmlC?si=8ySScaJrR1GJkAF9ZNgAaw', 0, current_date
+(), current_date
+());
+    
