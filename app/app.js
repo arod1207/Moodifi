966eb7004e72823dc1f925d177e8d57d115fd067
Jaylan <jaylanwienckowski@gmail.com>
2020-07-24 12:17:19 -0500

    adding activity card

--- app/app.js
+++ app/app.js
@@ -1,34 +1,34 @@
 const express = require("express");
 const db = require("./models");
 const app = express();
-const Handlebars = require("handlebars");
-const expressHandlebars = require("express-handlebars");
-const {
-  allowInsecurePrototypeAccess,
-} = require("@handlebars/allow-prototype-access");
+var exphbs = require("express-handlebars");
+var path = require ('path');
 
 const PORT = process.env.PORT || 3060;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.engine(
-  "handlebars",
-  expressHandlebars({
-    handlebars: allowInsecurePrototypeAccess(Handlebars),
-  })
-);
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 // app.set("views", path.join(__dirname, "views"))
 // console.log("blah", __dirname)
 app.use(express.static("public"));
 
-const router = require("./routes/playlistAPI");
-app.use(router);
+
+const happyAPI = require("./routes/playlistAPI");
+app.use("/api", happyAPI);
+
+const sadAPI = require("./routes/playlistAPI");
+app.use("/api", sadAPI);
+
+const angryAPI = require("./routes/playlistAPI");
+app.use("/api", angryAPI);
 
 app.use("/", (req, res) => {
   res.render("index");
 });
+
 
 db.sequelize.sync().then(() => {
   app.listen(PORT, () => {
