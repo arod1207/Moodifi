9d3b827b6825dd30fce4fda88da0f9958c5df493
Arod1207 <arod1207@me.com>
2020-07-26 22:26:10 -0500

    Updated seed file

--- app/routes/playlistAPI.js
+++ app/routes/playlistAPI.js
@@ -2,22 +2,6 @@ const express = require("express");
 const router = express.Router();
 const db = require("../models");
 
-var rating;
-
-// GET LINKS AND RATINGS //
-// router.get("/playlist/:category/:name", (req, res) => {
-//   db.playlist.findOne({
-//     where: {
-//       CATEGORY: req.params.category,
-//       NAME: req.params.name
-//     }
-//   }).then((playlist) => {
-//     console.log(playlist)
-//     res.render("index", {
-//       playlist
-//     });
-//   })
-// });
 
 router.get("/playlist/moods/all", (req, res) => {
   db.playlist.findAll({
@@ -45,17 +29,17 @@ router.get("/playlist/activities/all", (req, res) => {
   })
 });
 
-router.post("/playlist/:category/:name", (req, res) => {
-  db.playlist.update(
-    {RATING: rating++},
-    {where: {
-      CATEGORY: req.params.category,
-      NAME: req.params.name
-    }}
-  ).then((playlist) => {
-    res.render('index', {
-      playlist
-    })
-  })
-});
+// router.post("/playlist/:category/:name", (req, res) => {
+//   db.playlist.update(
+//     {RATING: rating++},
+//     {where: {
+//       CATEGORY: req.params.category,
+//       NAME: req.params.name
+//     }}
+//   ).then((playlist) => {
+//     res.render('index', {
+//       playlist
+//     })
+//   })
+// });
 module.exports = router;
