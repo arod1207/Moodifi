e74554736304ed2690294eda295252404f8d7adf
Armando Rodriguez <arod1207@me.com>
2020-07-27 13:01:28 -0500

    added dislike button updated seed file and playlist model

--- app/public/assets/js/rating.js
+++ app/public/assets/js/rating.js
@@ -1,15 +1,6 @@
 $(document).ready(function(){
 
-  // $("#increment").click(function (e) {
-  //   $.ajax({
-  //     url: '/playlist/:id',
-  //     success: function(data) {
-  //       if (data == "refresh"){
-  //         window.location.reload(); // This is not jQuery but simple plain ol' JS
-  //       }
-  //     }
-  //   });
-  // });
+
   });
 
 
