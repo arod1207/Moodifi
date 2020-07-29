c8a8077856bf6c027edf7fbdc223965d62f5d28d
Armando Rodriguez <arod1207@me.com>
2020-07-28 20:35:12 -0500

    cleaning up code in passport.js

--- app/config/passport.js
+++ app/config/passport.js
@@ -2,42 +2,41 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const db = require("../models");
 const bcrypt = require("bcryptjs");
-const e = require("express");
+// const e = require("express");
 
-passport.use(new LocalStrategy(
-  
-  {
-    usernameField: "username"
-  },
-   function(username, password, done) {
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "username",
+    },
+    function (username, password, done) {
+      db.User.findOne({
+        where: {
+          USERNAME: username,
+        },
+      }).then(async (dbUser) => {
+        console.log(dbUser);
+        if (dbUser === null) {
+          return done(null, false, {
+            message: "Incorrect Username.",
+          });
+        } else if (await bcrypt.compare(password, dbUser.PASSWORD)) {
+          return done(null, dbUser);
+        } else {
+          return done(null, false, {
+            message: "Password Incorrect",
+          });
+        }
+      });
+    }
+  )
+);
 
-    db.User.findOne({
-      where: {
-        USERNAME: username
-      }
-    }).then( async (dbUser) => {
-      console.log(dbUser)
-      if (dbUser === null) {
-        return done(null, false, {
-          message: "Incorrect Username."
-        });
-      }
-      else if (await bcrypt.compare(password, dbUser.PASSWORD)) {
-        return done(null, dbUser) 
-      } else {
-        return done(null, false, {
-          message: "Password Incorrect"
-        })
-      }
-    });
-  }
-));
-
-passport.serializeUser(function(user, cb) {
+passport.serializeUser(function (user, cb) {
   cb(null, user);
 });
 
-passport.deserializeUser(function(obj, cb) {
+passport.deserializeUser(function (obj, cb) {
   cb(null, obj);
 });
 
--- app/models/User.js
+++ app/models/User.js
@@ -1,5 +1,3 @@
-// const bcrypt = require("bcryptjs");
-
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("User", {
     // The email cannot be null, and must be a proper email before creation
