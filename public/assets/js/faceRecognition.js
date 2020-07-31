e39188069aaab30bd211ffb0480e5ee14bd96247
Armando Rodriguez <arod1207@me.com>
2020-07-31 12:08:01 -0500

    face recognition

--- public/assets/js/faceRecognition.js
+++ public/assets/js/faceRecognition.js
@@ -1,44 +1,47 @@
 // possibly use later //
 
-var base_64 = $("#base64").val();
-
 function setup() {
-  $(".submitBtn").on("click", function (e) {
+  $(".turnOnBtn").click(function (e) {
     e.preventDefault();
-
     noCanvas();
-    const video = createCapture(VIDEO);
+    video = createCapture(VIDEO);
+    video.position(150,150);
+    video.center();
+    filter(THRESHOLD);
+    image(video, 0, 0);
 
-    var url = $("#base64").val();
-    console.log(url);
-    var settings = {
-      async: true,
-      crossDomain: true,
-      url: `https://faceplusplus-faceplusplus.p.rapidapi.com/facepp/v3/detect?return_attributes=emotion&image_base64=${url}`,
-      method: "POST",
-      headers: {
-        "x-rapidapi-host": "faceplusplus-faceplusplus.p.rapidapi.com",
-        "x-rapidapi-key": process.env.XRAPID_KEY,
-        "content-type": "application/x-www-form-urlencoded",
-      },
-      data: {},
-    };
+    $(".picBtn").click(function (e) {
+      e.preventDefault();
+      video.loadPixels();
+      const image64 = video.canvas.toDataURL();
 
-    $.ajax(settings).done(function (response) {
-      console.log(response);
-      let anger = response.faces[0].attributes.emotion.anger;
-      let happiness = response.faces[0].attributes.emotion.happiness;
-      let neutral = response.faces[0].attributes.emotion.neutral;
+      const api_url = "https://api-us.faceplusplus.com/facepp/v3/detect";
 
-      if (anger > 50) {
-        window.location = "/playlist/moods/angry";
-      } else if (neutral > 50) {
-        window.location = "/playlist/moods/sad";
-      } else if (happiness < 50) {
-        window.location = "/playlist/moods/sad";
-      } else {
-        window.location = "/playlist/moods/happy";
-      }
+      $.ajax({
+        url: api_url,
+        method: "POST",
+        data: {
+          api_key: "mp3ITqttnJPUUI45s1ugOfJgDEc4JmH0",
+          api_secret: "6bQwRGbsaITtTpesTh6pH_ftFKsJJNT2",
+          image_base64: `${image64}`,
+          return_attributes: "emotion",
+        },
+      }).done((data) => {
+        console.log(data);
+        let anger = data.faces[0].attributes.emotion.anger;
+        let happiness = data.faces[0].attributes.emotion.happiness;
+        let neutral = data.faces[0].attributes.emotion.neutral;
+
+        if (anger > 50) {
+          window.location = "/playlist/moods/angry";
+        } else if (neutral > 50) {
+          window.location = "/playlist/moods/sad";
+        } else if (happiness < 50) {
+          window.location = "/playlist/moods/sad";
+        } else {
+          window.location = "/playlist/moods/happy";
+        }
+      });
     });
   });
 }
