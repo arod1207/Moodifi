6330a426c192340252e0d2edcb98ab5ed728e6d4
Arod1207 <arod1207@me.com>
2020-07-31 04:39:17 -0500

    added a image row in db

--- models/User.js
+++ models/User.js
@@ -1,20 +1,18 @@
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
   var User = sequelize.define("User", {
-    // The email cannot be null, and must be a proper email before creation
     USERNAME: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true,
-      unique: {
-        args: true,
-        msg: 'Username already in use'
-    }
     },
-    // The password cannot be null
     PASSWORD: {
       type: DataTypes.STRING,
-      allowNull: false
-    }
+      allowNull: false,
+    },
+    IMAGE: {
+      type: DataTypes.STRING,
+      unique: true,
+    },
   });
   return User;
 };
