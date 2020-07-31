3f2203195bb3816d215fad15d3cb6e85aa66d307
Arod1207 <arod1207@me.com>
2020-07-31 05:35:03 -0500

    webcam turning on

--- public/assets/js/faceRecognition.js
+++ public/assets/js/faceRecognition.js
@@ -1,41 +1,44 @@
 // possibly use later //
 
-// var url = $("#url").val();
+var base_64 = $("#base64").val();
 
-// $(".submitBtn").on("click", function (e) {
-//   e.preventDefault();
+function setup() {
+  $(".submitBtn").on("click", function (e) {
+    e.preventDefault();
 
-//   var url = $("#url").val();
-//   console.log(url);
-//   var settings = {
-//     async: true,
-//     crossDomain: true,
-//     url: `https://faceplusplus-faceplusplus.p.rapidapi.com/facepp/v3/detect?return_attributes=emotion&image_url=${url}`,
-//     method: "POST",
-//     headers: {
-//       "x-rapidapi-host": "faceplusplus-faceplusplus.p.rapidapi.com",
-//       "x-rapidapi-key": process.env.XRAPID_KEY,
-//       "content-type": "application/x-www-form-urlencoded",
-//     },
-//     data: {},
-//   };
+    noCanvas();
+    const video = createCapture(VIDEO);
 
-//   $.ajax(settings).done(function (response) {
-//     console.log(response);
-//     let anger = response.faces[0].attributes.emotion.anger;
-//     let happiness = response.faces[0].attributes.emotion.happiness;
-//     let neutral = response.faces[0].attributes.emotion.neutral;
+    var url = $("#base64").val();
+    console.log(url);
+    var settings = {
+      async: true,
+      crossDomain: true,
+      url: `https://faceplusplus-faceplusplus.p.rapidapi.com/facepp/v3/detect?return_attributes=emotion&image_base64=${url}`,
+      method: "POST",
+      headers: {
+        "x-rapidapi-host": "faceplusplus-faceplusplus.p.rapidapi.com",
+        "x-rapidapi-key": process.env.XRAPID_KEY,
+        "content-type": "application/x-www-form-urlencoded",
+      },
+      data: {},
+    };
 
-//     if (anger > 50) {
-//       window.location = "/playlist/moods/angry";
-//     } else if (neutral > 50) {
-//       window.location = "/playlist/moods/sad";
-//     } else if (happiness < 50) {
-//       window.location = "/playlist/moods/sad";
-//     } else {
-//       window.location = "/playlist/moods/happy";
-//     }
-    
-//   });
-//   document.getElementById("url").value = '';
-// });
+    $.ajax(settings).done(function (response) {
+      console.log(response);
+      let anger = response.faces[0].attributes.emotion.anger;
+      let happiness = response.faces[0].attributes.emotion.happiness;
+      let neutral = response.faces[0].attributes.emotion.neutral;
+
+      if (anger > 50) {
+        window.location = "/playlist/moods/angry";
+      } else if (neutral > 50) {
+        window.location = "/playlist/moods/sad";
+      } else if (happiness < 50) {
+        window.location = "/playlist/moods/sad";
+      } else {
+        window.location = "/playlist/moods/happy";
+      }
+    });
+  });
+}
